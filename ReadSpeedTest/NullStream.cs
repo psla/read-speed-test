a333e7c32427ca8d58a3ab38bb52f31d0acd94ba
Piotr (Peter) Slatala <psla-github@sepio.pl>
2021-01-05 16:53:56 -0800

    C# ReadSpeedTest

---
+++ ReadSpeedTest/NullStream.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+
+namespace ReadSpeedTest
+{
+    internal class NullStream : Stream
+    {
+        private long length;
+        private long position;
+
+        public NullStream()
+        {
+        }
+
+        public override bool CanRead => true;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => true;
+
+        public override long Length => length;
+
+        public override long Position { get { return position; } set { throw new System.NotImplementedException(); } }
+
+        public override void Flush()
+        {
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            length += count;
+            position += count;
+        }
+    }
+}
---
+++ ReadSpeedTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ReadSpeedTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var swTotal = new Stopwatch();
+            var swAfterOpen = new Stopwatch();
+
+            // Compile File.OpenRead
+            using (var test = File.OpenRead(@"C:\trac_msi.log")) ;
+            
+            swTotal.Start();
+            {
+                using (var fs = File.OpenRead(args[0]))
+                {
+                    swAfterOpen.Start();
+
+                    using (var outputStream = new NullStream()) {
+                        fs.CopyTo(outputStream);
+                        swAfterOpen.Stop();
+                        swTotal.Stop();
+                        Console.WriteLine("{0}", args[0]);
+                        Console.WriteLine("After open:  {0}", PrintDetails(swAfterOpen, outputStream.Length));
+                        Console.WriteLine("Before open: {0}", PrintDetails(swTotal, outputStream.Length));
+                    }
+
+                }
+            }
+        }
+
+        static string PrintDetails(Stopwatch sw, long numBytes)
+        {
+            return string.Format("{0} bytes ({1:0} MB) copied, {2:0.000} s, {3:0.0} MB/s", numBytes, numBytes / 1024.0 / 1024, sw.ElapsedMilliseconds / 1000.0, (double) numBytes / (sw.ElapsedMilliseconds / 1000.0) / 1024 / 1024);
+        }
+    }
+}
